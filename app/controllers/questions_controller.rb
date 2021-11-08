dfda0706645072b223dd647f5fd2c202be131f7e
massinissa aouchiche <aouchiche.massinissa@gmail.com>
2021-11-08 14:16:32 +0100

    stupid coaching challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,11 +10,11 @@ class QuestionsController < ApplicationController
     end
     private
 
-    def coach_answer(@question)
-      if @question.include?("?")
+    def coach_answer(question)
+      if question.end_with?("?")
         return "Silly question, get dressed and go to work!"
-      elsif @question == "I am going to work right now!"
-        return ""
+      elsif question == "I am going to work right now!"
+        return "Great"
       else
         return "I don't care, get dressed and go to work!"
       end
